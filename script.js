15d8001d92aa9ac2d135e846be6d6eee0ae9dba7
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-07-12 23:28:35 -0400

    Added variables and basic prompts to begin the set up of my code

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment code here
 
+var capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWQYZ";
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var specialCharacters = "`~!@#$%^&*()_+<>?/-,.";
+var numbers = "0123456789";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -12,6 +16,48 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+var generatePassword = function() {
+  var userChoice = prompt("Select a password length between 8 and 128 characters");
+  // var tryAgain = prompt("Make sure to choose a number between 8 and 128 characters"); 
+ if (userChoice < 8 || userChoice > 128 || null) {
+  alert("Make sure to choose a number between 8 and 128 characters"); 
+  return;
+} 
+
+ var newPassword = "";
+
+ var addCapital = confirm("Should the password have capital letters?");
+ if (addCapital) {
+  newPassword += password;
+  console.log(addCapital);
+  // return newPassword;
+ }
+
+ var addLower = confirm("Should the password have lowercase letters?");
+ if (addLower) {
+  newPassword += password;
+  console.log(addLower);
+  // return newPassword;
+ }
+
+ var addNumbers = confirm("Should the password have numbers?");
+ if (addNumbers) {
+  newPassword += password;
+  console.log(addNumbers);
+  // return newPassword;
+ }
+
+ var addSpecial = confirm("Should the password have special characters?");
+ if (addSpecial) {
+  newPassword += password;
+  console.log(addSpecial);
+  // return newPassword;
+ }
+
+}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
