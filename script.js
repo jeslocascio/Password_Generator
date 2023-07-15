1b915b16cf7f5c81635bede7b1780b9bfe05bef9
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-07-15 16:29:50 -0400

    Added comments to the javascript file

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment code here
 
+// Sets variables for different character sets
 var capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWQYZ";
 var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 var specialCharacters = "`~!@#$%^&*()_+<>?/-,.";
@@ -17,57 +18,70 @@ function writePassword() {
 
 }
 
+// Generate a password based on user preferences
 var generatePassword = function() {
   var userChoice = 0;
 
+  // Uses a prompt to set the length of a password between 8 and 128 characters
   var userChoice = prompt("Select a password length between 8 and 128 characters");
-  
- if (userChoice < 8 || userChoice > 128 || null || isNaN(userChoice)) {
-  alert("Make sure to choose a number between 8 and 128 characters"); 
-  return ("Your password is too short!");
-} 
 
- var newPassword = "";
+  // Checks if the inputed character meets the criteria
+  if (userChoice < 8 || userChoice > 128 || null || isNaN(userChoice)) {
+    // Displays an alert if the inputed character doesn't meet the criteria
+    alert("Make sure to choose a number between 8 and 128 characters");
+    return ("Your password didn't meet the criteria!");
+  }
 
- var addCapital = confirm("Should the password have capital letters?");
- if (addCapital) {
-  newPassword = capitalLetters.concat(newPassword);
- }
+  // Creates an empty string as the basis for the new password
+  var newPassword = "";
 
- var addLower = confirm("Should the password have lowercase letters?");
- if (addLower) {
-  newPassword = lowercaseLetters.concat(newPassword);
- }
+  // Asks the user if the password should include capital letters
+  var addCapital = confirm("Should the password have capital letters?");
+  if (addCapital) {
+    // Adds capital letters to the potential character pool
+    newPassword = capitalLetters.concat(newPassword);
+  }
 
- var addNumbers = confirm("Should the password have numbers?");
- if (addNumbers) {
-  newPassword = numbers.concat(newPassword);
+  // Asks the user if the password should include lowercase letters
+  var addLower = confirm("Should the password have lowercase letters?");
+  if (addLower) {
+    // Adds lower letters to the potential character pool
+    newPassword = lowercaseLetters.concat(newPassword);
+  }
 
- }
+  // Asks the user if the password should include numbers
+  var addNumbers = confirm("Should the password have numbers?");
+  if (addNumbers) {
+   // Adds numbers to the potential character pool
+    newPassword = numbers.concat(newPassword);
+  }
 
- var addSpecial = confirm("Should the password have special characters?");
- if (addSpecial) {
-  newPassword = specialCharacters.concat(newPassword);
- }
+  // Asks the user if the password should include special characters
+  var addSpecial = confirm("Should the password have special characters?");
+  if (addSpecial) {
+    // Adds lower letters to the potential character pool
+    newPassword = specialCharacters.concat(newPassword);
+  }
 
-
- 
- if ( !addCapital , !addLower , !addSpecial , !addNumbers)
-  {
-    alert(
-      "It's not secure to have a blank password!"
-    );
+  // Checks if the user hasn't selected any character types to the password
+  if (!addCapital && !addLower && !addNumbers && !addSpecial) {
+    // Displays an alert and resets the page so you can try again
+    alert("It's not secure to have a blank password!");
     window.location.reload();
   }
 
- let arr = Array.from(newPassword);
- console.log(arr);
- let result = "";
+  // Converts the new password from a string to an array
+  let arr = Array.from(newPassword);
+  console.log(arr);
+  let result = "";
 
- for (let i = 0; i < userChoice; i++) {
-  result += arr[Math.floor(Math.random() * arr.length)];
- }
- return result;
+  // Generates the password by randomly selecting characters from the array
+  for (let i = 0; i < userChoice; i++) {
+    result += arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  // Prints the generated password in the text box on the page
+  return result;
 };
 
 
